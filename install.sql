440e1ce3974b2e9f662890056ffbbafa2f00621c
akters <62516931+akters@users.noreply.github.com>
2020-04-02 01:57:53 +0700

    Update install.sql

--- install.sql
+++ install.sql
@@ -13,7 +13,6 @@ CREATE TABLE Author
   Author VARCHAR(50) NOT NULL,
   Title VARCHAR(50) NOT NULL,
   Details VARCHAR(50) NOT NULL,
-  FOREIGN KEY (Barcode) REFERENCES Bookdetails(Barcode),
 );
 
 CREATE TABLE Genre
@@ -22,18 +21,7 @@ CREATE TABLE Genre
 );
 
 
-insert into Bookdetails values("Oliver Twist", "Charles Dickens","978-8-41-740605-9","978-8-41-740605-9-21561","DA-21561")
-insert into Author values("Aristotle","Organon","Click here")
-insert into Genre values("Action and adventure")
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+insert into Bookdetails values("Oliver Twist", "Charles Dickens","978-8-41-740605-9","978-8-41-740605-9-21561","DA-21561");
+insert into Author values("Aristotle","Organon","Click here");
+insert into Genre values("Action and adventure");
+
