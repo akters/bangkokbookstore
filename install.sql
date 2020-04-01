f402e97112598c1d696fbb805a276c3090eff7bf
akters <62516931+akters@users.noreply.github.com>
2020-04-02 01:44:54 +0700

    Update install.sql

--- install.sql
+++ install.sql
@@ -18,14 +18,13 @@ CREATE TABLE Author
 
 CREATE TABLE Genre
 (
-  Fiction VARCHAR(50) NOT NULL,
-  Non-fiction VARCHAR(50) NOT NULL,
+  Genre VARCHAR(50) NOT NULL,
 );
 
 
 insert into Bookdetails values("Oliver Twist", "Charles Dickens","978-8-41-740605-9","978-8-41-740605-9-21561","DA-21561")
 insert into Author values("Aristotle","Organon","Click here")
-insert into Genre values("Action and adventure","Art")
+insert into Genre values("Action and adventure")
 © 2020 GitHub, Inc.
 Terms
 Privacy
