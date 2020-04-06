d18de046c4fe89bf0f184eb0fc0c60b6a1fef179
akters <62516931+akters@users.noreply.github.com>
2020-04-07 00:56:55 +0700

    Create BookstoreAuthor.php

---
+++ BookstoreAuthor.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">BookStore</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="BookstoreHome.php">Home</a></li>
+      <li><a href="BookstoreAbout.php">About</a></li>
+      <li><a href="BookstoreBook.php">Book</a></li>
+      <li class="active"><a href="#">Author</a></li>
+      <li><a href="BookstoreGenre.php">Genre</a></li>
+      <li><a href="https://drive.google.com/file/d/1DQXZCknmbRYo3j5hrfzwQdBJMe5H9781/view?usp=sharing"></i>HELP</a></li>
+       <li><a href="#">Contact Us</a></li>
+     
+      <form name ="form1" action="" method="post">
+      <input type ="submit" name ="Suggest_Book" value ="Suggest Book">
+      </form>
+      
+      <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit">Submit</button>
+      </form>
+      
+    </ul>
+  </div>
+</nav>
+  <style>
+.center {
+  text-align: center;
+  border: 3px solid black;
+}
+</style>
+     <div class="center">
+  <h2>Author</h2>
+</div>
+</table>
+<div class="container">
+<h4>Select a letter </h4>
+ <div id="nav" class="CharacterContainer"></div>
+  <table class="table table-bordered">
+
+ <div class="container">
+  <div class="btn-toolbar">
+    <div class="btn-group btn-group-black">
+      <div class="btn">
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/a"><button>A</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/b"><button>B</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/c"><button>C</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/d"><button>D</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/e"><button>E</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/f"><button>F</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/g"><button>G</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/h"><button>H</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/i"><button>I</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/j"><button>J</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/k"><button>K</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/l"><button>L</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/m"><button>M</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/n"><button>N</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/o"><button>O</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/p"><button>P</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/q"><button>Q</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/r"><button>R</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/s"><button>S</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/t"><button>T</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/u"><button>U</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/v"><button>V</button></a>           
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/w"><button>W</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/x"><button>X</button></a>
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/y"><button>Y</button></a> 
+      <a href=\"https://www.bookbrowse.com/authors/allauthors/index.cfm/fuseaction/long_view/author_last_name_starts_with/z"><button>Z</button></a> 
+      </div> 
+      
+      
+  <thead>
+  <tr>
+  <th>Author</th>
+    <th>Title</th>
+      <th>Details</th>
+         </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td> <a href=\"https://schoolworkhelper.net/oliver-twist-characters-setting-style-audience-and-diction/">Aristotle  </a> </td>
+        <td>Organon</td>
+        <td>Click here</td>
+      
+        </tr>
+        <tr>
+        <td>Arthur Conan Doyle</td>
+        <td>Sherlock Holmes</td>
+        <td>Clich here</td>
+        </tr>
+         <tr>
+        <td>Arthur Conan Doyle</td>
+        <td> The Napoleonic Tales</td>
+        <td>Click here</td>
+        </tr>
+          <tr>
+        <td>Arthur Conan Doyle</td>
+        <td> The Professor Challenger works</td>
+        <td>Click here</td>
+        </tr>
+          <tr>
+        <td>Arthur Conan Doyle</td>
+        <td> The Mystery of Cloomber</td>
+        <td>Click here</td>
+        </tr>
+          <tr>
+        <td>Arthur Conan Doyle</td>
+        <td>Micah Clarke</td>
+        <td>Click here</td>
+        </tr>
+    </tbody>
+  </table>
+ 
+    </tbody>
+  </table>
+</div>
+</body>
+</html>
