53c6cd3cf51ce211b3cdf947d73510369525d72b
akters <62516931+akters@users.noreply.github.com>
2020-04-12 23:28:01 +0700

    Update install.sql

--- install.sql
+++ install.sql
@@ -36,19 +36,9 @@ CREATE TABLE Book_type_Description
   FOREIGN KEY (Type_num) REFERENCES Book_type(Type_num)
 );
 
-CREATE TABLE Book_Suggestion
-(
-  Sug_Title VARCHAR(30) NOT NULL,
-  Sug_Author VARCHAR(30) NOT NULL,
-  Sug_ISBN VARCHAR(30) NOT NULL,
-  Sug_Type_num INT NOT NULL,
-  Sug_Req_num INT NOT NULL,
-  PRIMARY KEY (Sug_Req_num)
- );
  
 
 insert into Book_type values (16);
 insert into Inventory values (15, 32, 62, "5A");
 insert into Book values ("Jane Eyre","Charlotte Bronte","1792739478", "1792739478218",32, 16, "5A");
 insert into Book_type_Description values ("life story of an orphan","thegreatestbooks.org/items/107",16);
-insert into Book_Suggestion values ("Harry Potter","JK Rowling","9066710972", 32, 150);
