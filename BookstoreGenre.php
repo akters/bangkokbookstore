aeb75a51a832ec8ab07d092176dadaf62c2d5387
akters <62516931+akters@users.noreply.github.com>
2020-04-20 05:11:36 +0700

    Genre tab

---
+++ BookstoreGenre.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">BookStore</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="BookstoreHome.php">Home</a></li>
+      <li><a href="BookstoreAbout.php">About</a></li>
+      <li><a href="BookstoreBook.php">Book</a></li>
+      <li><a href="BookstoreAuthor.php">Author</a></li>
+      <li class="active"><a href="#">Genre</a></li>
+      <li><a href="https://drive.google.com/file/d/1DQXZCknmbRYo3j5hrfzwQdBJMe5H9781/view?usp=sharing"></i>HELP</a></li>
+      <li><a href="BookstoreContactus.php">Contact Us</a></li>
+     
+      <form name ="form1" action="" method="post">
+      <input type ="submit" name ="Add_Book.php" value ="Add Book">
+      </form>
+      
+      <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit">Submit</button>
+      </form>
+      
+    </ul>
+  </div>
+</nav>
+  <style>
+.center {
+  text-align: center;
+  border: 3px solid black;
+}
+</style>
+     <div class="center">
+  <h2>Genre</h2>
+</div>
+</table>
+<div class="container">
+
+<h4>Action</h4> 
+<h4>Adventure</h4>
+<h4>Art</h4>
+<h4>Autobiography</h4>
+<h4>Classic</h4>
+<h4>Comic</h4>
+<h4>Cookbook</h4>
+<h4>Crime</h4>
+<h4>Diary</h4>
+<h4>Drama</h4>
+<h4>Fairytale</h4>
+<h4>Fantasy</h4>
+<h4>Guide</h4>
+<h4>Health</h4>
+<h4>History</h4>
+<h4>Horror</h4>
+
+
+</div>
+</body>
+</html>
