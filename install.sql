016641b9b0e26f37193a7acfbbfea997a4dcc371
akters <62516931+akters@users.noreply.github.com>
2020-04-20 02:14:49 +0700

    Create Tables in Database

--- install.sql
+++ install.sql
@@ -1,3 +1,10 @@
+CREATE TABLE Book_type
+(
+  Type_num INT NOT NULL,
+  Description VARCHAR(30) NOT NULL,
+  Details_Url VARCHAR(30) NOT NULL,
+  PRIMARY KEY (Type_num)
+);
 
 CREATE TABLE Inventory
 (
@@ -5,35 +12,24 @@ CREATE TABLE Inventory
   Num_Ordered INT NOT NULL,
   Num_Sold INT NOT NULL,
   Shelf_num VARCHAR(30) NOT NULL,
-  Type_num INT NOT NULL,
-  PRIMARY KEY (Type_num)
+  Book_copy_num INT NOT NULL,
   PRIMARY KEY (Shelf_num)
 );
 
 CREATE TABLE Book
 (
-  Title VARCHAR(30) NOT NULL,
   Author VARCHAR(30) NOT NULL,
-  ISBN VARCHAR(30) NOT NULL,
+  Title VARCHAR(30) NOT NULL,
   Barcode VARCHAR(30) NOT NULL,
-  Book_Copy_Num INT NOT NULL,
+  ISBN VARCHAR(30) NOT NULL,
+  Book_copy_num INT NOT NULL,
   Type_num INT NOT NULL,
   Shelf_num VARCHAR(30) NOT NULL,
-  PRIMARY KEY (Book_Copy_Num),
-  FOREIGN KEY (Type_num) REFERENCES Inventory(Type_num),
+  PRIMARY KEY (Book_copy_num),
+  FOREIGN KEY (Type_num) REFERENCES Book_type(Type_num),
   FOREIGN KEY (Shelf_num) REFERENCES Inventory(Shelf_num)
 );
 
-CREATE TABLE Book_type_Description
-(
-  Description VARCHAR(30) NOT NULL,
-  Details_URL VARCHAR(100) NOT NULL,
-  Type_num INT NOT NULL,
-  PRIMARY KEY (Description, Type_num),
-  FOREIGN KEY (Type_num) REFERENCES  Inventory(Type_num)
-);
-
- 
-insert into Inventory values (15, 32, 62, "5A", 16);
-insert into Book values ("Jane Eyre","Charlotte Bronte","1792739478", "1792739478218",32, 16, "5A");
-insert into Book_type_Description values ("life story of an orphan","thegreatestbooks.org/items/107",16);
+insert into Book_type values (16, "Classical Novel", "thegreatestbooks.org/items/107");
+insert into Inventory values (12, 100, 3027, "5A", 316);
+insert into Book values ("Charlotte Bronte", "Jane Eyre", 218385809551, 2183858, 316, 16, "5A");
